ed8c4f9122eb142416fb5076f06fb8a1d9231edd
EvaP27 <63146664+EvaP27@users.noreply.github.com>
2020-05-20 21:13:58 +0200

    Chat

--- app/src/main/java/com/example/test_cont/Chat.java
+++ app/src/main/java/com/example/test_cont/Chat.java
@@ -3,16 +3,24 @@ package com.example.test_cont;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.android.material.tabs.TabLayout;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class Chat extends Fragment {
+
+    private ViewPager viewPager;
+    private TabLayout myTabLayout;
+    private TabsAccessorAdapter myTabsAccessorAdapter;
+
 
     public Chat() {
         // Required empty public constructor
@@ -22,7 +30,15 @@ public class Chat extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chat, container, false);
+        View v =inflater.inflate(R.layout.fragment_chat, container, false);
+
+        viewPager =v.findViewById(R.id.main_tabs_pager);
+        myTabsAccessorAdapter=new TabsAccessorAdapter(getFragmentManager());
+        viewPager.setAdapter(myTabsAccessorAdapter);
+
+        myTabLayout=v.findViewById(R.id.main_tabs);
+        myTabLayout.setupWithViewPager(viewPager);
+
+        return v;
     }
 }
